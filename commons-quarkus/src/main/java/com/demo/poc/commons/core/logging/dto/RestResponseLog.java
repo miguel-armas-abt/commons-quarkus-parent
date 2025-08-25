598afaf9bb53a281e6bb3c1afff3c4d23ce14c7d
Miguel <miguel.armas.abt@gmail.com>
2025-08-25 17:48:31 -0500

    First commit

---
+++ commons-quarkus/src/main/java/com/demo/poc/commons/core/logging/dto/RestResponseLog.java
@@ -0,0 +1,21 @@
+package com.demo.poc.commons.core.logging.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Map;
+
+@Builder
+@Getter
+@Setter
+public class RestResponseLog implements Serializable {
+
+  private String method;
+  private String httpCode;
+  private String uri;
+  private Map<String, String> responseHeaders;
+  private String responseBody;
+  private String traceParent;
+}
